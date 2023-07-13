f1749e3e81845a5455b2dc4a5103d876f0439e34
Tobias Skarhed <1438972+tskarhed@users.noreply.github.com>
2023-07-13 09:29:49 +0200

    Traces: Enable metrics generation from traces (#26)
    * Enable metrics generation from traces
    
    * Remove constant dimensions
    
    * Remove use of title and change the service to Drone
    
    * Remove service graph metrics
    
    * traces: add CI attributes
    
    * Set otel status codes
    
    * traces: only set error for code 1
    
    * traces: include traces for 1 previous h

--- pkg/dronereceiver/handler.go
+++ pkg/dronereceiver/handler.go
@@ -31,6 +31,21 @@ type droneWebhookHandler struct {
 	nextTraceConsumer consumer.Traces
 }
 
+const CI_KIND = "ci.kind"
+const CI_STAGE = "ci.stage"
+
+func getOtelExitCode(code int) ptrace.StatusCode {
+	if code == 0 {
+		return ptrace.StatusCodeOk
+	}
+
+	if code == 1 {
+		return ptrace.StatusCodeError
+	}
+
+	return ptrace.StatusCodeUnset
+}
+
 func (d *droneWebhookHandler) handler(resp http.ResponseWriter, req *http.Request) {
 	// TODO: this is just a stub for now
 	d.logger.Info("Got request")
@@ -51,9 +66,16 @@ func (d *droneWebhookHandler) handler(resp http.ResponseWriter, req *http.Reques
 	}
 
 	build := completedBuild.Build
+	repo := completedBuild.Repo
 
 	if build.Finished == 0 {
 		return
+	}
+
+	buildCode := 0
+
+	if build.Error != "" {
+		buildCode = 1
 	}
 
 	traceId := NewTraceID()
@@ -74,17 +96,21 @@ func (d *droneWebhookHandler) handler(resp http.ResponseWriter, req *http.Reques
 	scopeSpans.Scope().SetVersion("0.1.0")
 
 	resourceAttrs := resourceSpan.Resource().Attributes()
-	resourceAttrs.PutStr(conventions.AttributeServiceName, "build")
 	resourceAttrs.PutStr(conventions.AttributeServiceVersion, "0.1.0")
+	resourceAttrs.PutStr(conventions.AttributeServiceName, "drone")
 	resourceAttrs.PutInt("build.number", build.Number)
 	resourceAttrs.PutInt("build.id", build.ID)
+	resourceAttrs.PutStr("repo.name", repo.Name)
+	resourceAttrs.PutStr("repo.branch", repo.Branch)
 
 	buildSpan := scopeSpans.Spans().AppendEmpty()
 	buildSpan.SetTraceID(traceId)
 	buildSpan.SetSpanID(buildId)
 	buildSpan.SetParentSpanID(pcommon.NewSpanIDEmpty())
+	buildSpan.Attributes().PutStr(CI_KIND, "build")
+	buildSpan.Status().SetCode(getOtelExitCode(buildCode))
 
-	buildSpan.SetName(build.Title)
+	//buildSpan.SetName(build.Title)
 
 	buildSpan.SetStartTimestamp(pcommon.Timestamp(build.Created * 1000000000))
 	buildSpan.SetEndTimestamp(pcommon.Timestamp(build.Finished * 1000000000))
@@ -97,10 +123,12 @@ func (d *droneWebhookHandler) handler(resp http.ResponseWriter, req *http.Reques
 		stageSpan.Attributes().PutStr(conventions.AttributeServiceName, stage.Name)
 		stageSpan.Attributes().PutInt("stage.number", int64(stage.Number))
 
+		stageSpan.Status().SetCode(getOtelExitCode(stage.ExitCode))
 		stageSpan.SetName(stage.Name)
 		stageSpan.SetTraceID(traceId)
 		stageSpan.SetSpanID(stageId)
 		stageSpan.SetParentSpanID(buildId)
+		stageSpan.Attributes().PutStr(CI_KIND, "stage")
 
 		stageSpan.SetStartTimestamp(pcommon.Timestamp(stage.Started * 1000000000))
 		stageSpan.SetEndTimestamp(pcommon.Timestamp(stage.Stopped * 1000000000))
@@ -110,6 +138,10 @@ func (d *droneWebhookHandler) handler(resp http.ResponseWriter, req *http.Reques
 			stepSpan.SetTraceID(traceId)
 			stepSpan.SetParentSpanID(stageId)
 			stepSpan.SetSpanID(NewSpanID())
+			stepSpan.Attributes().PutStr(CI_KIND, "step")
+			stepSpan.Attributes().PutStr(CI_STAGE, stage.Name)
+
+			stepSpan.Status().SetCode(getOtelExitCode(step.ExitCode))
 
 			stepSpan.SetName(step.Name)
 
