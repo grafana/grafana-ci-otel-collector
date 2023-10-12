c4a89c08bfabb89f64fde8ea4189f3e09381800c
Giordano Ricci <me@giordanoricci.com>
2023-10-12 12:50:12 +0100

    semconv: add VCS conventions

--- pkg/semconv/resource.go
+++ pkg/semconv/resource.go
@@ -1,1 +1,38 @@
 package semconv
+
+// VCS Info
+const (
+	// VCS Type
+	//
+	// Type: Enum
+	// Requirement Level: Optional
+	// Stability: alpha
+	AttributeVCSType = "vcs.type"
+)
+
+// VCS Type Enum
+const (
+	AttributeVCSTypeGit = "git"
+)
+
+// Git repository info
+const (
+	// Git HTTP URL
+	//
+	// Type: string
+	// Requirement Level: Optional
+	// Stability: alpha
+	AttributeGitHTTPURL = "git.url.http"
+	// Git SSH URL
+	//
+	// Type: string
+	// Requirement Level: Optional
+	// Stability: alpha
+	AttributeGitSSHURL = "git.url.ssh"
+	// Git web URL
+	//
+	// Type: string
+	// Requirement Level: Optional
+	// Stability: alpha
+	AttributeGitWWWURL = "git.url.www"
+)
