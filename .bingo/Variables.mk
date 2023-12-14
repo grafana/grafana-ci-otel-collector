448a72cbccd654c646ac1ef11772d1c2cdbd1e33
Giordano Ricci <me@giordanoricci.com>
2023-12-14 13:13:54 +0000

    chore: update to otel 0.91.0

--- .bingo/Variables.mk
+++ .bingo/Variables.mk
@@ -1,4 +1,4 @@
-# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.8. DO NOT EDIT.
+# Auto generated binary variables helper managed by https://github.com/bwplotka/bingo v0.9. DO NOT EDIT.
 # All tools are designed to be build inside $GOBIN.
 BINGO_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 GOPATH ?= $(shell go env GOPATH)
@@ -23,15 +23,15 @@ $(BINGO): $(BINGO_DIR)/bingo.mod
 	@echo "(re)installing $(GOBIN)/bingo-v0.8.0"
 	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=bingo.mod -o=$(GOBIN)/bingo-v0.8.0 "github.com/bwplotka/bingo"
 
-BUILDER := $(GOBIN)/builder-v0.83.0
+BUILDER := $(GOBIN)/builder-v0.91.0
 $(BUILDER): $(BINGO_DIR)/builder.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/builder-v0.83.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=builder.mod -o=$(GOBIN)/builder-v0.83.0 "go.opentelemetry.io/collector/cmd/builder"
+	@echo "(re)installing $(GOBIN)/builder-v0.91.0"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=builder.mod -o=$(GOBIN)/builder-v0.91.0 "go.opentelemetry.io/collector/cmd/builder"
 
-MDATAGEN := $(GOBIN)/mdatagen-v0.83.0
+MDATAGEN := $(GOBIN)/mdatagen-v0.91.0
 $(MDATAGEN): $(BINGO_DIR)/mdatagen.mod
 	@# Install binary/ries using Go 1.14+ build command. This is using bwplotka/bingo-controlled, separate go module with pinned dependencies.
-	@echo "(re)installing $(GOBIN)/mdatagen-v0.83.0"
-	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=mdatagen.mod -o=$(GOBIN)/mdatagen-v0.83.0 "github.com/open-telemetry/opentelemetry-collector-contrib/cmd/mdatagen"
+	@echo "(re)installing $(GOBIN)/mdatagen-v0.91.0"
+	@cd $(BINGO_DIR) && GOWORK=off $(GO) build -mod=mod -modfile=mdatagen.mod -o=$(GOBIN)/mdatagen-v0.91.0 "github.com/open-telemetry/opentelemetry-collector-contrib/cmd/mdatagen"
 
