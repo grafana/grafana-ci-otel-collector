12604989f248b88d0dc17d25e6b1357a11b2e410
dsotirakis <dimitrios.sotirakis@grafana.com>
2024-11-07 16:34:41 +0200

    make generate

--- receiver/githubactionsreceiver/internal/metadata/generated_status.go
+++ receiver/githubactionsreceiver/internal/metadata/generated_status.go
@@ -4,12 +4,11 @@ package metadata
 
 import (
 	"go.opentelemetry.io/collector/component"
-	"go.opentelemetry.io/otel/metric"
-	"go.opentelemetry.io/otel/trace"
 )
 
 var (
-	Type = component.MustNewType("githubactions")
+	Type      = component.MustNewType("githubactions")
+	ScopeName = "github.com/grafana/grafana-ci-otel-collector/receiver/githubactionsreceiver"
 )
 
 const (
@@ -17,11 +16,3 @@ const (
 	LogsStability    = component.StabilityLevelAlpha
 	MetricsStability = component.StabilityLevelAlpha
 )
-
-func Meter(settings component.TelemetrySettings) metric.Meter {
-	return settings.MeterProvider.Meter("github.com/grafana/grafana-ci-otel-collector/receiver/githubactionsreceiver")
-}
-
-func Tracer(settings component.TelemetrySettings) trace.Tracer {
-	return settings.TracerProvider.Tracer("github.com/grafana/grafana-ci-otel-collector/receiver/githubactionsreceiver")
-}
