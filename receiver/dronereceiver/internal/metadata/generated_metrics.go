c63ac55b62900d22f6f1263ab65cf0c6694ce4b4
dsotirakis <dimitrios.sotirakis@grafana.com>
2025-06-06 16:14:15 +0300

    make generate

--- receiver/dronereceiver/internal/metadata/generated_metrics.go
+++ receiver/dronereceiver/internal/metadata/generated_metrics.go
@@ -9,7 +9,7 @@ import (
 	"go.opentelemetry.io/collector/pdata/pcommon"
 	"go.opentelemetry.io/collector/pdata/pmetric"
 	"go.opentelemetry.io/collector/receiver"
-	conventions "go.opentelemetry.io/collector/semconv/v1.9.0"
+	conventions "go.opentelemetry.io/otel/semconv/v1.9.0"
 )
 
 // AttributeCiWorkflowItemStatus specifies the value ci.workflow_item.status attribute.
